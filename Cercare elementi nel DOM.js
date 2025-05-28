a7386ff38d47b91f9427cb27489c1c85b1072802
Christian Sestito <sestitochristian4@gmail.com>
2025-05-28 17:10:15 +0200

    Cercare elementi nel DOM

---
+++ Cercare elementi nel DOM.js
@@ -0,0 +1,6 @@
+// Cerca tutti gli elementi con classe "paragrafo".
+const paragrafi = document.getElementsByClassName("paragrafo");
+// Cerca l'elemento con id "elemento-3"
+const elemento3 = document.getElementById("elemento-3");
+// Cerca tutti gli elementi h2 della pagina
+const h2 = document.getElementsByTagName("h2");
