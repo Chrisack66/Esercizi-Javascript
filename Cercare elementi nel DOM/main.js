3c3b5b4c797c732f9b2143bca0584e4208ded8c4
Christian Sestito <sestitochristian4@gmail.com>
2025-05-29 10:22:56 +0200

    Inserito codice javascript nel file html

--- Cercare elementi nel DOM/main.js
+++
@@ -1,6 +0,0 @@
-// Cerca tutti gli elementi con classe "paragrafo".
-const paragrafi = document.getElementsByClassName("paragrafo");
-// Cerca l'elemento con id "elemento-3"
-const elemento3 = document.getElementById("elemento-3");
-// Cerca tutti gli elementi h2 della pagina
-const h2 = document.getElementsByTagName("h2");
